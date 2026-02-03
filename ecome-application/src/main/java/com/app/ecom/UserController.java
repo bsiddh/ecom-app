348bb8f40a812aecbdcee0d3e6f1141dedeef4e9
bsiddh <bsiddh667@gmail.com>
2026-02-04 02:21:03 +0530

    response has been added

--- ecome-application/src/main/java/com/app/ecom/UserController.java
+++ ecome-application/src/main/java/com/app/ecom/UserController.java
@@ -1,11 +1,11 @@
 package com.app.ecom;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,13 +17,21 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/users")
-    public List<User> getAllUsers(){
-        return userService.fetchAllUsers();
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userService.fetchAllUsers(),HttpStatus.OK);
     }
 
     @PostMapping("/api/users")
-    public String createUsers(@RequestBody  User user){
+    public ResponseEntity<String> createUsers(@RequestBody  User user){
         userService.addUsers(user);
-        return "user added successfully";
+        return ResponseEntity.ok("user added successfully");
+    }
+
+    @GetMapping("/api/users/{id}")
+    public ResponseEntity<User> getUsers(@PathVariable Long id){
+        User user = userService.fetchUser(id);
+        if (user == null)
+       return    ResponseEntity.notFound().build();
+        return ResponseEntity.ok(user);
     }
 }
--- ecome-application/src/main/java/com/app/ecom/UserService.java
+++ ecome-application/src/main/java/com/app/ecom/UserService.java
@@ -19,4 +19,13 @@ public class UserService {
         userList.add(user);
         return userList;
     }
+
+    public User fetchUser(Long id) {
+        for (User user : userList){
+            if (user.getId().equals(id)){
+                return user;
+            }
+        }
+        return null;
+    }
 }
