c82954322cbce12704f2e71d07656739111fee72
bsiddh <bsiddh667@gmail.com>
2026-02-04 00:38:38 +0530

    main

---
+++ ecome-application/src/main/java/com/app/ecom/UserController.java
@@ -0,0 +1,29 @@
+package com.app.ecom;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/api/users")
+    public List<User> getAllUsers(){
+        return userService.fetchAllUsers();
+    }
+
+    @PostMapping("/api/users")
+    public String createUsers(@RequestBody  User user){
+        userService.addUsers(user);
+        return "user added successfully";
+    }
+}
---
+++ ecome-application/src/main/java/com/app/ecom/UserService.java
@@ -0,0 +1,20 @@
+package com.app.ecom;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private List<User> userList = new ArrayList<>();
+
+    public List<User> fetchAllUsers() {
+        return userList;
+    }
+
+    public List<User> addUsers(User user) {
+        userList.add(user);
+        return userList;
+    }
+}
